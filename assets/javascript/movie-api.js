dd50427a01e051d1b398d8af374e8fd68cb90740
michellechristina <michelle@thisisbam.com>
2017-11-03 17:04:53 -0400

    add movie-api.js

--- assets/javascript/movie-api.js
+++ assets/javascript/movie-api.js
@@ -1,1 +1,29 @@
-// javascript for movie api code
+var zipCode = "03867";
+var radius = 5;
+var d = new Date();
+var today = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
+
+
+
+var queryURL = "http://data.tmsapi.com/v1.1/movies/showings";
+queryURL += '?' + $.param({
+    'startDate': today,
+    'zip': zipCode,
+    'radius': radius,
+    'units': "mi",
+    'api_key': "rb8hzag4f93j2f86dbqbcrn5"
+});
+
+console.log(queryURL);
+
+
+// http://data.tmsapi.com/v1.1/movies/showings?api-key=rb8hzag4f93j2f86dbqbcrn5&zip=03867&radius=5&units=mi
+
+
+$.ajax({
+    url: queryURL,
+    method: 'GET',
+}).done(function (response) {
+    console.log(response);
+    // console.log(response.data[0].url); 
+});

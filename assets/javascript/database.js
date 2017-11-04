62399b8337487fe229300c423a25dbb4e8f1e470
janetcushing <janet.cushing@gmail.com>
2017-11-04 12:24:10 -0400

    committing changes

--- assets/javascript/database.js
+++ assets/javascript/database.js
@@ -14,8 +14,11 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 var dateDataRef = database.ref("/dateData");
-var dateDataQuery = dateDataRef.orderByKey();
+var dateDataQuery = database.ref("dateData").orderByChild("dateRating").limitToFirst(3);
+// var dateDataQuery = database.ref("dateData").orderByKey().limitToFirst(3);
 
+
+var zipCodeIn = '03801';
 {
     dateData = [
 
@@ -25,10 +28,11 @@ var dateDataQuery = dateDataRef.orderByKey();
             "movieTitle": "Frozen",
             "theatre": "Regal Fox Run Stadium 15",
             "times": ['4:00 PM', '7:00 PM', '9:00 PM'],
-            "theatreAddress": "45 Gosling Road, Newington, NH 03801",
+            "theatreaddress": "45 Gosling Road, Newington, NH 03801",
+            "theatreUrl"  : "https://www.regmovies.com/theaters/regal-fox-run-stadium-15-rpx/C00352134632",
             "restaurantName": "Lexies",
             "restaurantAddress": "212 Islington, Portsmouth, NH",
-            "restaurantLink": "https://www.peaceloveburgers.com/portsmouth",
+            "restaurantUrl": "https://www.peaceloveburgers.com/portsmouth",
             "dateRating": "awesome"
         },
 
@@ -38,10 +42,11 @@ var dateDataQuery = dateDataRef.orderByKey();
             "movieTitle": "Thor: Ragnarok",
             "theatre": "Cinematic Portsmouth",
             "times": ['4:00 PM', '7:00 PM', '9:00 PM'],
-            "theatreAddress": "2454 Lafayette Rd, Portsmouth, NH 03801",
+            "theatreaddress": "2454 Lafayette Rd, Portsmouth, NH 03801",
+            "theatreUrl"  : "http://www.cinemagicmovies.com/location/36613/Cinemagic-Stadium-10-in-Portsmouth-NH-Showtimes",
             "restaurantName": "Tuscan Kitchen",
             "restaurantAddress": "581 Lafayette Rd Portsmouth, NH 03801-5406",
-            "restaurantLink": "http://tuscanbrands.com/",
+            "restaurantUrl": "http://tuscanbrands.com/",
             "dateRating": "super duper"
         },
 
@@ -51,10 +56,11 @@ var dateDataQuery = dateDataRef.orderByKey();
             "movieTitle": "HAPPY DEATH DAY",
             "theatre": "BARNZ'S BARRINGTON CINEMA",
             "times": ['4:00 PM', '7:00 PM', '9:00 PM'],
-            "theatreAddress": "586 Calef Highway, Suite One, Barrington, NH 03825",
+            "theatreaddress": "586 Calef Highway, Suite One, Barrington, NH 03825",
+            "theatreUrl"  : "http://www.barnzs.com/",
             "restaurantName": "Dinosaur Bar-B-Que",
             "restaurantAddress": "99 Court St Rochester, NH 14604",
-            "restaurantLink": "http://www.dinobbq.com/",
+            "restaurantUrl": "http://www.dinobbq.com/",
             "dateRating": "fun"
         },
 
@@ -64,10 +70,11 @@ var dateDataQuery = dateDataRef.orderByKey();
             "movieTitle": "Loving Vincent",
             "theatre": "Portsmouth Music Hall",
             "times": ['7:00 PM'],
-            "theatreAddress": "28 Chestnut St, Portsmouth, NH 03801",
+            "theatreaddress": "28 Chestnut St, Portsmouth, NH 03801",
+            "theatreUrl"  : "http://www.themusichall.org/",
             "restaurantName": "The River House",
             "restaurantAddress": "53 Bow St Portsmouth, NH 03801",
-            "restaurantLink": "http://www.riverhouse53bow.com/",
+            "restaurantUrl": "http://www.riverhouse53bow.com/",
             "dateRating": "good times"
         },
 
@@ -77,12 +84,13 @@ var dateDataQuery = dateDataRef.orderByKey();
             "movieTitle": "THE FOREIGNER (2017)",
             "theatre": "SMITTY'S SANFORD CINEMA & PUB",
             "times": ['6:30 PM', '10:15 PM'],
-            "theatreAddress": "45 Gosling Road, Newington, NH 03801",
+            "theatreaddress": "45 Gosling Road, Newington, NH 03801",
+            "theatreUrl"  : "http://smittyscinema.com/sanford/",
             "restaurantName": "Revolution Taproom and Grill",
             "restaurantAddress": " 61 N Main St, Rochester, NH 03867",
-            "restaurantLink": "www.revolutiontaproomandgrill.com/",
+            "restaurantUrl": "www.revolutiontaproomandgrill.com/",
             "dateRating": "awesome"
-                },
+        },
 
         {
             "zipCode": "03839",
@@ -90,10 +98,11 @@ var dateDataQuery = dateDataRef.orderByKey();
             "movieTitle": "Geostorm",
             "theatre": "BARNZ'S BARRINGTON CINEMA",
             "times": ['4:00 PM', '7:00 PM', '9:00 PM'],
-            "theatreAddress": "586 Calef Highway, Suite One, Barrington, NH 03825",
+            "theatreaddress": "586 Calef Highway, Suite One, Barrington, NH 03825",
+            "theatreUrl"  : "http://www.barnzs.com/",
             "restaurantName": "The Garage at The Governor's Inn",
             "restaurantAddress": " 78 Wakefield St, Rochester, NH 03867",
-            "restaurantLink": "www.governorsinn.com/thegarage.cfm",
+            "restaurantUrl": "www.governorsinn.com/thegarage.cfm",
             "dateRating": "great"
         }
     ]
@@ -110,10 +119,11 @@ function updateDatabase() {
             movieTitle: dateData[i].movieTitle,
             theatre: dateData[i].theatre,
             times: dateData[i].times.toString(),
-            theatreAddress: dateData[i].theatreAddress,
+            theatreaddress: dateData[i].theatreaddress,
+            theatreUrl: dateData[i].theatreUrl,
             restaurantName: dateData[i].restaurantName,
             restaurantAddress: dateData[i].restaurantAddress,
-            restaurantLink: dateData[i].restaurantLink,
+            restaurantUrl: dateData[i].restaurantUrl,
             dateRating: dateData[i].dateRating
 
         });
@@ -124,40 +134,34 @@ function updateDatabase() {
 
 function getOutputFromDatabase() {
     console.log("im in getOutputFromDatabase ");
-        dateDataQuery.once("value").then(function(snap){
-            snap.forEach(function (snapshot) {
+    $("#pastDates").empty();
+    dateDataQuery.on("child_added", function (snapshot) {
+        console.log(snapshot.val().time);
+        var movieDiv = $("<div>");
+        var pTitle = $('<p class="movieOutput">Movie: ' + snapshot.val().movieTitle + '</p>');
+        var pTheatre = $('<p class="theatreOutput">Theatre: ' + snapshot.val().theatre + '</p>');
+        // var pTime = $('<p class="time">Times: ' + snapshot.val().time + '</p>');
+        var pMovieAddress = $('<p class="movieAddress">Movie: ' + snapshot.val().theatreAddress + '</p>');
+        var restaurantDiv = $("<div>");
+        var pRestaurantName = $('<p class="restaurantOutput">Restaurant: ' + snapshot.val().restaurant + '</p>');
+        var prestaurantUrl = $('<p class="restaurantUrlOutput">URL: ' + snapshot.val().restaurantUrl + '</p>');
+        var ratingDiv = $("<div>");
+        var dateRating = $('<p class="dateRatingOutput">Date Rating: ' + snapshot.val().dateRating + '</p>');
+        movieDiv.append(pTitle);
+        movieDiv.append(pTheatre);
+        // movieDiv.append(pTime);
+        movieDiv.append(pMovieAddress);
+        restaurantDiv.append(pRestaurantName);
+        restaurantDiv.append(prestaurantUrl);
+        ratingDiv.append(dateRating);
+        var date = $("<div>");
+        date.append(movieDiv);
+        date.append(restaurantDiv);
+        date.append(ratingDiv);
+        $("#pastDates").append(date);
+    });
 
-                console.log("in the snap");
-                var pTitle = $('<p class="movieOutput">Movie: ' + snapshot.val().movieTitle + '</p>');
-                var pTitle = $('<p>');
-                pTitle.addClass('movieOutput');
-                pTitle.text('Movie: ' + snapshot.val().movieTitle);
-
-                var pTheatre = $('<p class="theatreOutput">Theatre: ' + snapshot.val().theatre + '</p>');
-                var pTime = $('<p class="time">Times: ' + snapshot.val().time.toString() + '</p>');
-                var pMovieAddrese = $('<p class="movieAddress">Movie: ' + snapshot.val().theatreAddress + '</p>');
-                var pMovie = $("div");
-                var pRestaurant = $('<p class="restaurantOutput">Restaurant: ' + snapshot.val().restaurant + '</p>');
-                var pRestaurantLink = $('<p class="restaurantLinkOutput">URL: ' + snapshot.val().restaurantLink + '</p>');
-                var pRestaurant = $("div");
-                var dateRating = $('<p class="dateRatingOutput">Date Rating: ' + snapshot.val().dateRating + '</p>');
-
-                pMovie.append(pTitle);
-                pMovie.append(pTheatre);
-                pMovie.append(pTime);
-                pMovie.append(pmovieAddress);
-                pRestaurant.append(pRestaurant);
-                pRestaurant.append(pRestaurantLink);
-
-                var date = $("div");
-                date.append(pmovie);
-                date.append(prestaurant);
-                date.append(pdateRating);
-                // $("#pastDates").prepend(date);
-            });
-        });
-
-    }
+}
 
 
 
@@ -169,11 +173,13 @@ $(document).ready(function () {
 
 
     $("#dInput").on('click', function () {
+        event.preventDefault();
         updateDatabase();
     });
 
 
     $("#dOutput").on('click', function () {
+        event.preventDefault();
         console.log("output button was clicked");
         getOutputFromDatabase();
     });

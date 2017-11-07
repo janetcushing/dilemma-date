f19ecee5b5f6953cc423b14c4456b51aa040f8e9
Adam Magee <magee927@gmail.com>
2017-11-07 00:38:57 -0500

    initial commit of restaurant-api.js

---
+++ assets/javascript/restaurant-api.js
@@ -0,0 +1,43 @@
+//Thre script tag below needs to be in the html to make the conversion from zip code to lat/long coordinates
+//<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyApxgL2mMoiMcWrEvui6tLwqs6zD6T_E7Y"></script>
+
+
+function GetLocation() {
+    var geocoder = new google.maps.Geocoder();
+    var address = document.getElementById("dnd-zipcode-input").value;
+    var cuisinePref = document.getElementById("dnd-cuisine-menu").value;
+    geocoder.geocode({
+        'address': address
+    }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var latitude = results[0].geometry.location.lat();
+            var longitude = results[0].geometry.location.lng();
+            console.log("Latitude: " + latitude + "\nLongitude: " + longitude);
+
+            //This is the "ajax" call to the Zomato server to fetch 5 restaurants serving x cuisine within 15+/- miles of the zip code requested.
+            $.get(
+                "https://developers.zomato.com/api/v2.1/search?count=5&sort=cost&order=asc&lat=" + latitude + "&lon=" +
+                longitude +
+                "&radius=25000&cuisines=" + cuisinePref +
+                "&apikey=5c1a7ed52d0f6e28f3b6a0cbadd9284b",
+                function (data, status) {
+
+                    // take response data | slice off the first result | map over the result & create function to stick the restuarantdetails object you are building into restaurants variable
+
+                    var restaurants = data.restaurants.slice(0, 1).map(function (restaurantInfo) {
+                        var restaurantDetails = {};
+                        restaurantDetails.name = restaurantInfo.restaurant.name;
+                        restaurantDetails.location = restaurantInfo.restaurant.location.address;
+                        restaurantDetails.url = restaurantInfo.restaurant.url;
+
+                        // this returns your restaurantDetails object & stuffs it into restaurants variable
+                        return restaurantDetails;
+                    });
+
+                    console.log(restaurants[0]);
+                });
+        } else {
+            console.log("Request failed.")
+        }
+    });
+}
